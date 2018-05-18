bb197f4ce696e290a951305e554bb841fc2e7545
malcomhamelin <39401347+malcomhamelin@users.noreply.github.com>
2018-05-18 12:29:02 +0200

    Add files via upload

--- LastLight1805/src/app/modele/entity/AnimatedEntity.java
+++ LastLight1805/src/app/modele/entity/AnimatedEntity.java
@@ -1,13 +1,18 @@
 package app.modele.entity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import app.modele.field.Field;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
 public abstract class AnimatedEntity extends Entity {
 	
-	protected IntegerProperty pv;
-	protected int attaque;
-	protected int velocity;
+	private IntegerProperty pv;
+	private int attaque;
+	private int velocity;
+	private Field map;
 	
 	public AnimatedEntity(int x, int y, int pv, int att, int v) {
 		this.x = new SimpleIntegerProperty(x);
@@ -15,6 +20,9 @@ public abstract class AnimatedEntity extends Entity {
 		this.pv = new SimpleIntegerProperty(pv);
 		this.attaque = att;
 		this.velocity = v;
+		
+		CROSSABLE_TILES_LIST = new ArrayList<Integer>();
+		Collections.addAll(CROSSABLE_TILES_LIST, CROSSABLE_TILES);
 	}
 	
 	@Override
@@ -23,19 +31,39 @@ public abstract class AnimatedEntity extends Entity {
 	}
 	
 	public void moveLeft() {
-		
+		if (x.get() % 32 != 0)
+			x.set(x.get() - velocity);
+		else
+			if (x.get() > 31 && CROSSABLE_TILES_LIST.contains(map.getField()[this.getIndiceY()][this.getIndiceX()-1]))
+				x.set(x.get() - velocity);
 	}
 	
 	public void moveRight() {
-		
+		if (x.get() % 32 != 0)
+			x.set(x.get() + velocity);
+		else
+			if (x.get() > 31 && CROSSABLE_TILES_LIST.contains(map.getField()[this.getIndiceY()][this.getIndiceX()-1]))
+				x.set(x.get() + velocity);
 	}
 
 	public void moveDown() {
-		
+		if (y.get() % 32 != 0)
+			y.set(y.get() + velocity);
+		else
+			if (y.get() < 768 && CROSSABLE_TILES_LIST.contains(map.getField()[this.getIndiceY()+1][this.getIndiceX()]))
+				y.set(y.get() + velocity);
 	}
 	
 	public void moveUp() {
-		
+		if (y.get() % 32 != 0)
+			y.set(y.get() - velocity);
+		else
+			if (y.get() > 31 && CROSSABLE_TILES_LIST.contains(map.getField()[this.getIndiceY()-1][this.getIndiceX()]))
+				y.set(y.get() - velocity);
+	}
+	
+	public void setMap(Field f) {
+		this.map = f;
 	}
 
 }
--- LastLight1805/src/app/modele/entity/Player.java
+++ LastLight1805/src/app/modele/entity/Player.java
@@ -1,98 +1,13 @@
 package app.modele.entity;
 
-import app.modele.weapon.Weapon;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
 public class Player extends AnimatedEntity {
 
-	// LIST OF TODOs
-	// update - useBoots - useNecklace - interact
-	
-	// Attributes
-	private BooleanProperty boots;
-	private BooleanProperty necklace;
-	private IntegerProperty money; // Always >= 0
-	private IntegerProperty potion; // Always >= 0
-	private IntegerProperty equippedWeapon;
-	private ObservableList<Weapon> listWeapons;
-	
-	// Constructor
 	public Player(int x, int y, int pv, int att, int v) {
 		super(x, y, pv, att, v);
-		this.boots = new SimpleBooleanProperty(false);
-		this.necklace = new SimpleBooleanProperty(false);
-		this.money = new SimpleIntegerProperty(0);
-		this.potion = new SimpleIntegerProperty(0);
-		this.equippedWeapon = null;
-		this.listWeapons = FXCollections.observableArrayList();
 	}
 	
-	// TODO
 	public void update() {
 		
-	}
-
-	// Increment by 1 the number of equippedWeapon.
-	// Set it t 0 when equippedWeapon = listWeapons.size() -1
-	public void changeWeapon() {
-		if (this.equippedWeapon.getValue() < this.listWeapons.size() -1)
-			this.equippedWeapon.set(this.equippedWeapon.getValue()+1);
-		else
-			this.equippedWeapon.set(0);
-	}
-	
-	// Gain 1 PV if the player is not full pv (max 3)
-	// Use 1 potion
-	public void usePotion() {
-		if (this.pv.getValue() < 3 && this.potion.getValue() > 0) {
-			this.pv.set(pv.getValue() +1);
-			this.potion.set(potion.getValue() - 1);
-		}
-		
-	}
-	
-	// Can only recieve potion 1 by 1
-	public void recievePotion(){
-		this.potion.set(this.potion.getValue() + 1);
-	}
-	
-	// TODO
-	public void useBoots() {
-		if (this.boots.getValue()) {
-			
-		}
-	}
-	
-	// TODO
-	public void useNecklace() {
-		if (this.necklace.getValue()) {
-			
-		}
-	}
-	
-	// Recieve a weapon and add it to listWeapons
-	public void recieveWeapon(Weapon w) {
-		this.listWeapons.add(w);
-	}
-	
-	// TODO
-	public void interact() {
-		
-	}
-	
-	// Needs to recieve posivite value
-	public void recieveMoney(int v) {
-		this.money.set(this.money.getValue() + v);
-	}
-	
-	// Needs to recieve positive value
-	public void spendMoney(int v) {
-		this.money.set(this.money.getValue() - v);
 	}
 	
 }
