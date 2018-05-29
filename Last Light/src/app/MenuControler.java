c06516b45a759a4437a6987882e580c5b60e76fa
skerroue <sebastien.kerroue@gmail.com>
2018-05-29 16:48:19 +0200

    Ajout d'un menu lors du lancement

---
+++ Last Light/src/app/MenuControler.java
@@ -0,0 +1,47 @@
+package app.controler;
+
+import java.io.File;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class MenuControler {
+
+	@FXML
+    private Pane pane;
+	
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    void play(ActionEvent event) {
+
+    	try {
+			FXMLLoader loader = new FXMLLoader();
+			URL url = new File("src/app/vue/vueGraphique.fxml").toURI().toURL();
+			loader.setLocation(url);
+			System.out.println(loader.getLocation());
+			Pane root = new Pane(); 
+			root=loader.load();
+	     	//stage.setResizable(false);
+	     	pane.getScene().setRoot(root);
+	     	
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    @FXML
+    void quit(ActionEvent event) {
+        System.exit(0);
+    }
+
+}
