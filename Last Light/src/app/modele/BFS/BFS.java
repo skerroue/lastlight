e508c26f2cb739099c7954276e08cd344f96b48a
malcomhamelin <39401347+malcomhamelin@users.noreply.github.com>
2018-05-31 14:06:51 +0200

    Ajout de la classe BFS (pathfinding)

---
+++ Last Light/src/app/modele/BFS/BFS.java
@@ -0,0 +1,85 @@
+package app.modele.BFS;
+
+import java.util.ArrayList; 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import app.modele.entity.AnimatedEntity;
+import app.modele.entity.Player;
+import app.modele.field.Field;
+import app.modele.field.Tile;
+
+public class BFS {
+	
+	private Player player;
+	private Field field;
+	private Queue<Tile> queue;
+	private HashMap<Tile, Tile> parents;
+	private ArrayList<Tile> adjTiles;
+	
+	public BFS(Player player, Field field) {
+		this.player = player;
+		this.field = field;
+		this.queue = new LinkedList<>();
+		this.parents = new HashMap<Tile, Tile>();
+		this.adjTiles = new ArrayList<>();
+	}
+	
+	public void lancerBFS() {
+		int x = player.getIndiceX();
+		int y = player.getIndiceY();
+		
+		this.parents.clear();
+		this.queue.clear();
+		this.adjTiles.clear();
+	
+		this.queue.add(field.getNextTile(y, x));
+		this.parents.put(field.getNextTile(y, x), field.getNextTile(y, x));
+		
+		while (!queue.isEmpty()) {
+			Tile temp = queue.remove();
+			y = temp.getI();
+			x = temp.getJ();
+			
+			if (x > 0) {
+				this.adjTiles.add(field.getNextTile(y, x-1));
+			}
+			if (y > 0) {
+				this.adjTiles.add(field.getNextTile(y-1, x));
+			}
+			if (x < 24) {
+				this.adjTiles.add(field.getNextTile(y, x+1));
+			}
+			if (y < 24) {
+				this.adjTiles.add(field.getNextTile(y+1, x));
+			}
+			
+			for (int i = 0 ; i < adjTiles.size() ; i++) {
+				
+				if (!this.parents.containsKey(adjTiles.get(i))) {
+					this.parents.put(this.adjTiles.get(i), temp);
+					this.queue.add(this.adjTiles.get(i));
+				}
+				
+			}
+			
+			this.adjTiles.clear();
+		}
+		
+	}
+	
+	public Tile searchWay(AnimatedEntity entity) {
+		int x = entity.getIndiceX();
+		int y = entity.getIndiceY();
+		
+		Tile t = this.field.getNextTile(y, x);
+		
+		return this.parents.get(t);
+	}
+	
+	public HashMap<Tile, Tile> getParents() {
+		return parents;
+	}
+ 
+}
