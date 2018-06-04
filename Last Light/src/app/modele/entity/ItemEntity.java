bfc4c4c4ae84805febffddc7ed73461743db8c00
Sébastien Kerroué <sebastien.kerroue@gmail.com>
2018-06-04 12:04:53 +0200

    Add files via upload

---
+++ Last Light/src/app/modele/entity/ItemEntity.java
@@ -0,0 +1,28 @@
+package app.modele.entity;
+
+import app.modele.weapon.Lampe;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class ItemEntity extends InanimatedEntity {
+
+	public ItemEntity(int id, int x, int y) {
+		super(id);
+		this.x = new SimpleIntegerProperty(x);
+		this.y = new SimpleIntegerProperty(y);
+	}
+	
+	// Dumb af cette methode mais est construite dans l'id�e o� une arme spawnera une et une seule fois dans le jeu
+	public void interact(Player p) {
+		switch (this.id) {
+		case 5 :
+			if (p.getPotion().getValue() < 3) {
+				p.earnPotion();
+				this.die();
+			}
+			break;
+		default :
+			break;
+		}
+	}
+
+}
