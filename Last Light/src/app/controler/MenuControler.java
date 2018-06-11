5c6e9d46211a1db4978eeadc45a0a87750b567b4
Sébastien Kerroué <sebastien.kerroue@gmail.com>
2018-06-11 11:13:39 +0200

    Ajout de musiques

--- Last Light/src/app/controler/MenuControler.java
+++ Last Light/src/app/controler/MenuControler.java
@@ -2,17 +2,19 @@ package app.controler;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
-public class MenuControler {
+public class MenuControler implements Initializable{
 
 	@FXML
     private Pane panemenu;
@@ -24,13 +26,15 @@ public class MenuControler {
     private Button playButton;
 
     @FXML
+    private Media sound;
+    private MediaPlayer player;
+    
+    @FXML
     void play(ActionEvent event) {
-
+    	stopMusic();
+		setSound("game");
+		setPlayer();
     	try {
-    		Media sound = new Media(new File("src/music/game.mp3").toURI().toString());
-    		MediaPlayer player = new MediaPlayer(sound);
-    		player.setCycleCount(Timeline.INDEFINITE);
-    		player.play();
 			FXMLLoader loader = new FXMLLoader();
 			URL url = new File("src/app/vue/vueGraphique.fxml").toURI().toURL();
 			loader.setLocation(url);
@@ -38,8 +42,7 @@ public class MenuControler {
 			Pane root = new Pane(); 
 			root=loader.load();
 	     	panemenu.getScene().setRoot(root);
-	     	
-			
+			launchMusic();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -49,5 +52,29 @@ public class MenuControler {
     void quit(ActionEvent event) {
         System.exit(0);
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		setSound("menu");
+		setPlayer();
+		this.player.setCycleCount(Timeline.INDEFINITE);
+		launchMusic();
+	}
+	
+	private void launchMusic() {
+		this.player.play();
+	}
+	
+	private void stopMusic() {
+		this.player.stop();
+	}
+	
+	private void setSound(String i) {
+		this.sound = new Media(new File("src/music/" + i +".mp3").toURI().toString());
+	}
+	
+	private void setPlayer() {
+		this.player = new MediaPlayer(this.sound);
+	}
 
 }
