c2cbf28b5adb85fb5b540b1f85d6d7f6b04a2248
malcomhamelin <39401347+malcomhamelin@users.noreply.github.com>
2018-06-15 15:31:14 +0200

    Ajout classe ButtonView

--- Last Light/src/app/vue/entity/InanimatedEntityView.java
+++ Last Light/src/app/vue/entity/InanimatedEntityView.java
@@ -7,27 +7,13 @@ import javafx.scene.image.Image;
 
 public class InanimatedEntityView extends EntityView {
 	
-	private InanimatedEntity inanimatedEntity;
+	protected InanimatedEntity inanimatedEntity;
 
 	public InanimatedEntityView(InanimatedEntity i) {
 		super(i);
 		this.inanimatedEntity = i;
 		
 		this.setImage(new Image("file:src/img/" + this.inanimatedEntity.getId() + ".png"));
-		
-		if (this.inanimatedEntity.getId().equals("button"))
-			this.inanimatedEntity.isInteracting().addListener(new ChangeListener<Boolean>() {
-
-				@Override
-				public void changed(ObservableValue<? extends Boolean> arg0, Boolean arg1, Boolean arg2) {
-					setGif();
-				}
-				
-			});
-	}
-	
-	public void setGif() {
-		this.setImage(new Image("file:src/img/" + this.inanimatedEntity.getId() + "-gif.gif"));
 	}
 	
 }
