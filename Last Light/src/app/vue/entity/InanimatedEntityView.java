5a01daaff7f92cdca994ee73cd5907b86d986f9a
malcomhamelin <39401347+malcomhamelin@users.noreply.github.com>
2018-06-15 14:58:55 +0200

    Ajout gif boutton

--- Last Light/src/app/vue/entity/InanimatedEntityView.java
+++ Last Light/src/app/vue/entity/InanimatedEntityView.java
@@ -1,6 +1,8 @@
 package app.vue.entity;
 
-import app.modele.entity.inanimated.InanimatedEntity; 
+import app.modele.entity.inanimated.InanimatedEntity;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.image.Image;
 
 public class InanimatedEntityView extends EntityView {
@@ -12,6 +14,20 @@ public class InanimatedEntityView extends EntityView {
 		this.inanimatedEntity = i;
 		
 		this.setImage(new Image("file:src/img/" + this.inanimatedEntity.getId() + ".png"));
-	}
+		
+		if (this.inanimatedEntity.getId().equals("button"))
+			this.inanimatedEntity.isInteracting().addListener(new ChangeListener<Boolean>() {
 
+				@Override
+				public void changed(ObservableValue<? extends Boolean> arg0, Boolean arg1, Boolean arg2) {
+					setGif();
+				}
+				
+			});
+	}
+	
+	public void setGif() {
+		this.setImage(new Image("file:src/img/" + this.inanimatedEntity.getId() + "-gif.gif"));
+	}
+	
 }
